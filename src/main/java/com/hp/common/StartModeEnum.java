9c7a09ad03e4cb3386c16fdffd0dab5e4e8ab41d
dengxy <dengxy@YFB-DENGXY.sumpay.local>
2018-06-21 17:50:47 +0800

    修改

--- src/main/java/com/hp/common/StartModeEnum.java
+++ src/main/java/com/hp/common/StartModeEnum.java
@@ -3,7 +3,7 @@ package com.hp.common;
 public enum StartModeEnum {
 	COMMON_AUTO("01"),COMMON_LIST("02"),COMMON_DETAIL("03"),COMMON_ERROR("04"),
 	AGENT("11"),AGENT_LIST("12"),AGENT_DETAIL("13"),AGENT_ERROR("14"),
-	WEBMAGIC_AUTO("21");
+	WEBMAGIC_AUTO("21"),WEBMAGIC_AGENT_AUTO("31");
 	
 	private String desc;
 
